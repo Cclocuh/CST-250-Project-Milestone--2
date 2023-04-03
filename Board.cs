5d3b7aa4c8400c5787628ba32fb61a2f2defbb31
Cclocuh <118848644+Cclocuh@users.noreply.github.com>
2023-04-02 18:28:02 -0700

    Add files via upload

---
+++ Board.cs
@@ -0,0 +1,167 @@
+﻿using SharpDX.Direct3D11;
+using System;
+using System.Collections.Generic;
+using MilestoneConsoleApp;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using SharpDX.DXGI;
+
+namespace MilestoneConsoleApp
+{
+    public class Board
+    {
+        public int BOARD_SIZE { get; set; }
+
+        public cell[,] theGrid;
+
+        public object CELL_WIDTH { get; set; }
+        public object CELL_HEIGHT { get; set; }
+
+        public struct cell
+        {
+            public bool hasBomb;
+            public bool hasFlag;
+            public bool isUncovered;
+            public int neighboringBombs;
+            public Texture2D texture;
+            public int X;
+            public int Y;
+
+            public cell(int r, int c) : this()
+            {
+                R = r;
+                C = c;
+            }
+
+            // 2d array of cell objects 
+            public cell[,] theGrid { get; set; }
+            public object position { get; set; }
+            //public object CELL_WIDTH { get; internal set; }
+            public int R { get; }
+            public int C { get; }
+        }
+
+        public Board()
+        {
+            
+            const int BOARD_SIZE = 10;
+            const int CELL_WIDTH = 48;
+            const int CELL_HEIGHT = 96;
+            cell[,] cell = new cell[BOARD_SIZE + 2, BOARD_SIZE + 2];
+            Texture2D[] numbers = new Texture2D[9];
+
+
+        }
+
+        public Board(int BOARD_SIZE)
+        {
+            //InitializeBoard();
+            PlantBombs();
+            CountNeighbors();
+        }
+
+        public void InitializeBoard(int BOARD_SIZE)
+        {
+            for (int r = 0; r < BOARD_SIZE; r++)
+            {
+                for (int c  = 0; c < BOARD_SIZE; c++)
+                {
+                    Texture2D[] numbers = new Texture2D[10];
+                    cell[,] cell = new cell[r, c];
+                    cell[r, c].hasBomb = false;
+                    cell[r, c].hasFlag = false;
+                    cell[r, c].isUncovered = false;
+                    cell[r, c].neighboringBombs = 0;
+                    cell[r, c].position = CELL_WIDTH;
+                    cell[r, c].position = CELL_HEIGHT;
+                }
+            }
+
+            // we must initialize the array to avoid Null exception errors.
+            theGrid = new cell[BOARD_SIZE, BOARD_SIZE];
+            for (int r = 0; r < BOARD_SIZE; r++)
+            {
+                for (int c = 0; c < BOARD_SIZE; c++)
+                {
+                    theGrid[r, c] = new cell(r, c);
+                }
+            }
+        }
+
+        void PlantBombs()
+        {
+            Random rand = new Random();
+            bool[] n = new bool[100];
+
+            for (int i = 0; i < 90; i++)
+                n[i] = false;
+
+            for (int i = 0; i < 100; i++)
+                n[i] = true;
+
+            for (int i = 0; i < 100; i++)
+            {
+                int pos = rand.Next(100);
+                bool save = n[i];
+                n[i] = n[pos];
+                n[pos] = save;
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                int c = i % 10;
+                int r = i / 10;
+
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (n[i])
+                    Console.WriteLine("*");
+                else
+                    Console.WriteLine(".");
+                if ((i + 1) % 10 == 0)
+                    Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+        }
+
+        void CountNeighbors()
+        {
+            cell[,] cell = new cell[BOARD_SIZE, BOARD_SIZE];
+            for (int r = 0; r < BOARD_SIZE; r++)
+            {
+                for (int c = 0; c < BOARD_SIZE; c++)
+                {
+                    int Count = 0;
+                    if (cell[r - 1, c - 1].hasBomb)
+                        Count++;
+                    if (cell[r - 1, c].hasBomb)
+                        Count++;
+                    if (cell[r - 1, c + 1].hasBomb)
+                        Count++;
+                    if (cell[r, c - 1].hasBomb)
+                        Count++;
+                    if (cell[r, c + 1].hasBomb)
+                        Count++;
+                    if (cell[r + 1, c - 1].hasBomb)
+                        Count++;
+                    if (cell[r + 1, c].hasBomb)
+                        Count++;
+                    if (cell[r + 1, c + 1].hasBomb)
+                        Count++;
+                    cell[r, c].neighboringBombs = Count;
+                }
+            }
+        }
+
+        public void MarkNextLegalMoves(Cell currentCell, string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using MilestoneConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MilestoneFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static public Board myBoard = new Board(10);
+        public Button[,] btnGrid = new Button[myBoard.BOARD_SIZE, myBoard.BOARD_SIZE];
+  
+        public Form1()
+        {
+            InitializeComponent();
+            populateGrid();
+        }
+
+        public void populateGrid()
+        {
+            int buttonBOARD_SIZE = panel1.Width / myBoard.BOARD_SIZE; // calculate the width of each button on the Grid 
+            panel1.Height = panel1.Width; 
+
+            // nested loop. Create buttons and place them  in the Panel
+            for (int r = 0; r < myBoard.BOARD_SIZE; r++)
+            {
+                for (int c = 0; c < myBoard.BOARD_SIZE; c++)
+                {
+                    btnGrid[r, c] = new Button();
+
+                    // make each button square 
+                    btnGrid[r, c].Width = buttonBOARD_SIZE;
+                    btnGrid[r, c].Height = buttonBOARD_SIZE;
+
+                    btnGrid[r, c].Click += Grid_Button_Click; // Add the same click event to each button.
+                    panel1.Controls.Add(btnGrid[r, c]); 
+                    btnGrid[r, c].Location = new Point(buttonBOARD_SIZE * r, buttonBOARD_SIZE * c);
+
+                    // the Tag attribute will hold the row and column number in a string
+                    btnGrid[r, c].Tag = r.ToString() + "|" + c.ToString();
+                }
+            }
+
+        }
+
+        private void Grid_Button_Click(object sender, EventArgs e)
+        {
+            // get the row and column number of the button just clicked.
+            string[] strArr = (sender as Button).Tag.ToString().Split('|');
+            int r = int.Parse(strArr[0]);
+            int c = int.Parse(strArr[1]);
+
+            UpdateButtonLabels();
+
+            // reset the background color of all buttons to the default (original) color.
+            for (int i = 0; i < myBoard.BOARD_SIZE; i++)
+            {
+                for (int j = 0; j < myBoard.BOARD_SIZE; j++)
+                {
+                    btnGrid[i, j].BackColor = default(Color);
+                }
+            }
+
+            // set the background color of the clicked button to something different. 
+            (sender as Button).BackColor = Color.Cornsilk;
+        }
+
+        public void UpdateButtonLabels()
+        {
+
+
+            for (int r = 0; r < myBoard.BOARD_SIZE; r++)
+            {
+                for (int c = 0; c < myBoard.BOARD_SIZE; c++)
+                {
+                    btnGrid[r, c].Text = "";
+                }
+            }
+        }
+    }
+}
